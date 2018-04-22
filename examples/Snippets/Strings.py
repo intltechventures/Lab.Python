4b37ec4e43cb864401d8a12528c52537d81c2e6b
Kelvin D. Meeks <kmeeks@intltechventures.com>
2018-04-21 19:23:15 -0700

    created file

---
+++ examples/Snippets/Strings.py
@@ -0,0 +1,11 @@
+
+print("Exploring Strings - single or double quotes, result in different objects being created")
+
+x = "I am a double quoted string"
+print ("x, id(x): ", x, ", ", id(x))
+y = 'I am a single quoted string'
+print ("y, id(y): ", y, ", ", id(y))
+x2 = "I am a double quoted string"
+print("x2, id(x2): ", x2, ", ", id(x2))
+
+

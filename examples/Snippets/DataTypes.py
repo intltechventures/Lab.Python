d1a33cc710535aa9c953f80c7696276f99247a3c
Kelvin D. Meeks <kmeeks@intltechventures.com>
2018-04-21 19:07:46 -0700

    created file

---
+++ examples/Snippets/DataTypes.py
@@ -0,0 +1,43 @@
+
+print("Exploring Data Types in Python: Octal (base 8) numbers")
+x = 0o10
+print("Decimal 8 = Octal 0o10: ", x)
+x = 0o07
+print("Decimal 7 = Octal 0o07: ", x)
+x = 0o20
+print("Decimal 16 = Octal 0o20: ", x)
+
+
+
+print("Exploring Data Types in Python: Hex (base 16) numbers")
+x = 0x10
+print("Decimal 16 = Hex 0x10: ", x)
+x = 0x20
+print("Decimal 32 = Hex 0x20: ", x)
+x = 0xFF
+print("Decimal 255 = Hex 0xFF: ", x)
+
+
+
+print("Exploring Data Types in Python: Integers - Python 3 integers can be of unlimited in size") 
+x = (256)**2**2**2**2 
+print("(256)**2**2**2**2: ", x) 
+
+
+print("Exploring Data Types in Python: Floating Point Numbers")
+x = 6.1986e-2
+print("Floating Point 6.1986e-2: ", x)
+
+
+
+print("Exploring Data Types in Python: Complex Numbers")
+a = 5 + 6j
+b = 7 - 2j
+c = a + b
+print("(5+6j)+(7-2j) = ", c)
+
+
+
+
+
+

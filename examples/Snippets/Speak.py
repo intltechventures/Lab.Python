ee46588a296388cde8ac840fcb9704eb231beaf0
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-03-15 22:59:31 -0700

    added additional library notes to test

--- examples/Snippets/Speak.py
+++ examples/Snippets/Speak.py
@@ -1,8 +1,9 @@
 import os
-import comtypes
 
 # Requires pip3 install comtypes
 # Tested with comtypes-1-17 
+# https://pypi.org/project/comtypes/
+import comtypes
 
 print("Beginning test of Microsoft Speech API")
 
@@ -11,3 +12,46 @@ print("Beginning test of Microsoft Speech API")
 from comtypes.client import CreateObject
 engine = CreateObject("SAPI.SpVoice")
 engine.speak("Hello World")
+
+
+
+# using pyttsx3 - Offline text to speech for python3
+# https://pythonprogramminglanguage.com/text-to-speech/
+# https://pyttsx3.readthedocs.io/en/latest/
+# https://github.com/nateshmbhat/pyttsx3
+# 
+# ****  error says that engine isn't found...?
+# import pyttsx
+# engine = pyttsx.init()
+# engine.say("I will speak this text")
+# engine.runAndWait()
+
+
+
+
+# Using IBM Waton TTS 
+# pip install tts-watson
+#
+# **** ERROR during install...
+# 
+# TBD...
+# 
+
+
+
+
+# Using Google Speech API
+# https://pythonspot.com/speech-recognition-using-google-speech-api/comment-page-1/
+#
+# TBD...
+# 
+
+
+
+# Google Web to Speech (via Browser)
+# https://www.google.com/intl/en/chrome/demos/speech.html
+# 
+# TBD...
+# 
+
+

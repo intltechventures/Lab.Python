08ffe953c956ecc788c3e8f2c18428a278c516aa
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-06-27 15:53:19 -0700

    more structure clean-up and comments

--- install_modules.bat
+++ install_modules.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
+cls
 REM Prepared by:
 REM   Kelvin D. Meeks
 REM   kmeeks@intltechventures.com
@@ -7,106 +8,122 @@ REM   https://www.intltechventures.com
 REM
 REM References:
 REM   https://packaging.python.org/tutorials/installing-packages/#upgrading-packages
+REM     General sytax for installing/Upgrading a library
+REM      ```pip install --upgrade SomeProject```
 REM
-REM   https://github.com/intltechventures/Lab.Python
 REM
 REM This file:
 REM   https://github.com/intltechventures/Lab.Python/blob/master/install_modules.bat
-
-
+REM
 REM 
-REM install_modules.bat
-REM A simple batch command file to autoamte the install process for library dependencies (in the Example programs) - or which are frequently/commonly/often used...
-REM
+REM Description: 
+REM   A simple batch command file to autoamte the install process for library dependencies (in the Example programs) - or which are frequently/commonly/often used...
 
 
-REM
+
+REM ************************************************************************************************************
 REM Update pip
 REM
-ECHO About to check pip for updates
-pause 
+ECHO Checking pip for updates
 python -m pip install --upgrade pip
 
 
-REM
-REM Here's what's installed curently
+
+REM ************************************************************************************************************
+REM Here's what's installed curently...
 REM
 pip list
+
+REM Verify installed packages have compatible dependencies...
 REM
-REM Review current package versions
-REM
-ECHO Pausing to review current installed package versions
-ECHO
+pip check
+
+ECHO Pausing to review current installed package versions & pip check results
 pause
-cls
-
-REM
-REM General sytax for installing/Upgrading a library
-REM
-REM pip install --upgrade SomeProject
 
 
-REM
-REM comtypes
+
+REM ************************************************************************************************************
+REM Instal/Upgrade Package: comtypes
 REM
 REM NOTE:
 REM   A dependency for pyttsx3
 REM
-REM a lightweight Python COM package, based on the ctypes FFI library, in less than 10000 lines of code (not counting the tests).
-REM allows to a Python program define, call, and implement custom and dispatch-based COM interfaces in pure Python. It works on Windows, 64-bit Windows, and Windows CE.
-REM
+REM Usage:
+REM   A lightweight Python COM package, based on the ctypes FFI library, in less than 10000 lines of code (not counting the tests).
+REM   allows to a Python program define, call, and implement custom and dispatch-based COM interfaces in pure Python. 
+REM   Works on Windows, 64-bit Windows, and Windows CE.
+
+cls
 ECHO Install/Upgrade comtypes
-pause
-cls
 pip install --upgrade comtypes
+pause
 
 
-REM 
-REM for Text-to-Speech demos
+
+REM ************************************************************************************************************
+REM Install/Updgrade Package: pyttsx3
 REM
+REM Usage:  
+REM   Text-to-Speech demos
+
+cls
 ECHO Install/Upgrade pyttsx3
-pause
-cls
 pip install --upgrade pyttsx3
+pause
 
 
-REM 
-REM numpy 
+
+REM ************************************************************************************************************
+REM Install/Upgrade Package:  numpy 
 REM
+
+cls
 ECHO Install/Upgrade numpy
-pause
-cls
 pip install --upgrade numpy
+pause
 
 
 
-REM 
-REM TensorFlow 2.0 Alpha
+REM ************************************************************************************************************
+REM Install/Upgrade Package: TensorFlow 2.0 Alpha
 REM
+
+cls
 ECHO Install/Upgrade tensorflow 2.0.0-alpha0
-pause
-cls
 pip install --upgrade tensorflow==2.0.0-alpha0
-
-
-REM 
-REM jsonschema
-REM
-REM https://pypi.org/project/jsonschema/
-REM
-ECHO Install/Upgrade jsonschema
-ECHO
 pause
+
+
+REM ************************************************************************************************************
+REM Install/Upgrade Package: jsonschema
+REM
+REM Referneces:
+REM   https://pypi.org/project/jsonschema/
+
 cls
+ECHO Install/Upgrade jsonschema
 pip install --upgrade jsonschema
+pause
 
 
-REM
+
+REM ************************************************************************************************************
+REM Verify installed packages have compatible dependencies...
+
+cls
+pip check
+
+ECHO Pausing to review current installed package versions
+pause 
+
+
+
+REM ************************************************************************************************************
 REM Review package update results
-REM
+
 cls
 ECHO Review Package Update Results
-ECHO
-python -m pip install --upgrade pip
+pip list
 
 

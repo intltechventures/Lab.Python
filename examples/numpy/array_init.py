0f52d796de09734c5d738f549a15788a509d0e41
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-06-27 23:07:34 -0700

    initial commit

---
+++ examples/numpy/array_init.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+print("initialize arry with three [1]s")
+print(np.ones(3))
+
+print("initialize arry with three [0]s")
+print(np.zeros(3))
+
+print("initialize arry with three random numbers")
+print(np.random.random(3))

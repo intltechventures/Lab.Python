bff1b1608c7f97fa070f672ecf8e68ee46661091
Kelvin D. Meeks <kmeeks@intltechventures.com>
2019-06-27 23:12:11 -0700

    initial commit

---
+++ examples/numpy/array_addition.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+data = np.array([1,2,3,4,5,6])
+print("data array: ")
+print(data)
+
+ones = np.ones(6)
+print("ones array:")
+print(ones)
+
+
+print("array addition: data + ones")
+print(data + ones)
+
